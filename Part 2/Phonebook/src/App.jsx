560da020e257313e9574e11ac22b032ab86587cd
TabSimone <79592276+TabSimone@users.noreply.github.com>
2024-09-10 21:29:01 +0200

    2.9

--- Part 2/Phonebook/src/App.jsx
+++ Part 2/Phonebook/src/App.jsx
@@ -2,19 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number : '12341434'
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  const handleChangeFilter = (event) => {
+    setNewFilter(event.target.value);
+  };
 
   const handleChangeNumber = (event) => {
     setNewNumber(event.target.value);
   };
 
-  const handleChangeName= (event) => {
+  const handleChangeName = (event) => {
     setNewName(event.target.value);
   };
 
@@ -47,25 +53,33 @@ const App = () => {
       setNewNumber('');
       setNewName('');
 
-
-      
-
     }
   };
 
   const Contacts = () => {
+    const contactList = persons
+      .filter(item => newFilter === '' || item.name.startsWith(newFilter))
+      .map(item => (
+        <li key={item.name}>
+          {item.name} {item.number}
+        </li>
+      ));
+  
+    return <ul>{contactList}</ul>;
+  };
 
-    const contactList = persons.map((item) => <li key={item.name}>{item.name} {item.number}</li>)
-
-    return contactList
-
-  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Filter</h2>
       <form onSubmit={handleSubmit}>
-      <div>number: <input  type="text" value={newNumber} onChange={handleChangeNumber} /></div> 
+        <div>filter show on: <input type="text" value={newFilter} onChange={handleChangeFilter} /></div>
+        <div>
+        </div>
+      </form>
+      <h2>Add New Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <div>number: <input type="text" value={newNumber} onChange={handleChangeNumber} /></div>
         <div>
           name:         <input type="text" value={newName} onChange={handleChangeName} />
         </div>
