bb0566c3c690aceb59cacbb89d3417696161b8a7
TabSimone <79592276+TabSimone@users.noreply.github.com>
2024-09-17 19:00:42 +0200

    3.5

--- Part 3/PhoneBook/index.js
+++ Part 3/PhoneBook/index.js
@@ -60,6 +60,36 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body.name)
+
+  
+  if (!body)  {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
